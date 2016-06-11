b5e49a920ba057d3357254a6787ec8476c0878d0
Francisco Dominguez <francisco.dominguez@urjc.es>
2016-06-11 12:25:27 +0200

    Distance threshold added

--- pyEasyARS1/src/EasyASR1.py
+++ pyEasyARS1/src/EasyASR1.py
@@ -16,11 +16,12 @@ class EasyASR(object):
     classdocs
     '''
 
-
-    def __init__(self, filePath="/home/francisco/voz"):
+    def __init__(self, filePath="/home/francisco/voz",ldis=0.15,dthres=49):
         '''
         Constructor
         '''
+        self.ldis=ldis #length discrimination rate a 15% work fine
+        self.distThreshold=dthres #distance threshold 
         basepath=filePath
         self.mfccs={}
         mfccAll=[]
@@ -32,18 +33,27 @@ class EasyASR(object):
             mfcc_feat1 = mfcc(sig1,rate1)
             self.mfccs[n]=mfcc_feat1
             
-    #return the kNN where k=1
+    #return the kNN where k=1 dscriminating by length
     def get1NN(self,mfcc_feat2):
         minrd=1e40
+        mkrd="NONE"
         for k in sorted(self.mfccs.keys()):
             mfcc_feat1=self.mfccs[k]
-            distance, path = fastdtw(mfcc_feat1, mfcc_feat2, dist=euclidean)
-            rd=distance/len(path) #Normalize distance bi path length
-            print k,distance, len(path),rd
-            if rd<minrd:
-                minrd=rd
-                mkrd=k
-        return mkrd,minrd
+            l1=len(mfcc_feat1)
+            l2=len(mfcc_feat2)
+            #discriminating by length
+            if abs(l1-l2)<l2*self.ldis:
+                distance, path = fastdtw(mfcc_feat1, mfcc_feat2, dist=euclidean)
+                rd=distance/len(path) #Normalize distance bi path length
+                #print k,distance,len(mfcc_feat2),len(mfcc_feat1), len(path),rd
+                print k,len(mfcc_feat2),len(mfcc_feat1),rd
+                if rd<minrd:
+                    minrd=rd
+                    mkrd=k
+        if minrd>self.distThreshold:
+            return "NONE",minrd
+        else:
+            return mkrd,minrd
     
     def record(self):
         os.system("sox -r 16000 -t alsa default recording.wav silence 1 0.1 1% 1 1.5 1%")
@@ -58,7 +68,9 @@ class EasyASR(object):
         self.record()
         return self.recognize("recording.wav")
     
-asr=EasyASR()
-print asr.recognizeFromMic()
+if __name__ == '__main__':
+    asr=EasyASR()
+    while True:
+        print asr.recognizeFromMic()
 
         
